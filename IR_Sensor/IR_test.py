ac9a44854c410a2ae38f6d73559f280093aab487
phancongtai2k2 <phantaiem18022002@gmail.com>
2024-05-28 18:04:32 +0700

    ManhToan update

--- IR_Sensor/IR_test.py
+++ IR_Sensor/IR_test.py
@@ -1,27 +1,23 @@
 import RPi.GPIO as GPIO
 import time
 
-sensor = 16
+sensor = 16  # Chan GPIO23, chan vat 16
 
+GPIO.setmode(GPIO.BOARD)  # Thiet lap che do danh so chan GPIO theo so do chan vat ly
+GPIO.setup(sensor, GPIO.IN)  # Thiet lap chan GPIO23 (sensor) la dau vao (INPUT)
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(sensor,GPIO.IN)
-
-
-
-print ("IR Sensor Ready.....")
-print (" ")
+print("IR Sensor Ready.....")  # In thong bao cam bien da san sang hoat dong
+print(" ")  # In ra mot dong trong
 
 try: 
-   while True:
-      if GPIO.input(sensor):
-          
-          print ("Object Detected")
-          while GPIO.input(sensor):
-              time.sleep(1)
-      else:
-          print ("No Object Detected")
+    while True:  # Vong lap vo han de lien tuc kiem tra trang thai cua cam bien
+        if GPIO.input(sensor):  # Kiem tra trang thai cua chan cam bien
+            print("Object Detected: 0")  # In ra gia tri 0 khi khong phat hien vat can
+            while GPIO.input(sensor):  # Vong lap ben trong de kiem tra trang thai cua cam bien
+                time.sleep(0.2)  # Cho 200 ms truoc khi kiem tra lai
+        else:  
+            print("No Object Detected: 1")  # In ra gia tri 1 khi co phat hien vat can
+            time.sleep(0.2)  # Cho 200 ms truoc khi kiem tra lai
 
-
-except KeyboardInterrupt:
-    GPIO.cleanup()
+except KeyboardInterrupt:  # Bat ngoai le khi nguoi dung nhan Ctrl+C de dung chuong trinh
+    GPIO.cleanup()  # Dat lai trang thai cua cac chan GPIO
